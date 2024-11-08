aeaee462e1aa5676497eb59cfa0bf748f235fa6b
Ahsan Khan <afzalahsankhan283@gmail.com>
2024-11-08 10:45:16 +0500

    update app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+// "use client"
 import React from "react"
 import Header from "@/components/Header"
 import Home from "@/components/Home"
 import Typewriter from "@/components/Typewriter";
 import Footer from "@/components/Footer"
 
-export default function page(){
+export default function Page(){
      return(
         <div>
        <Header/>
